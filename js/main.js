fda590211b528069ef34b4c973509612cce9930e
konstantin0s <constantintofan85@gmail>
2018-02-05 21:40:26 +0100

    Display inputs form in the page

--- js/main.js
+++ js/main.js
@@ -11,7 +11,7 @@ function saveBookmark(e){
     name: siteName,
     url: siteUrl
   }
-  
+
  //Local storage test
  // localStorage.setItem('test', 'hello world');
  // console.log(localStorage.getItem('test'));
@@ -23,8 +23,38 @@ if(localStorage.getItem('bookmarks') === null){
   bookmarks.push(bookmark);
   //Set to localStorage
   localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+} else {
+  //Get bookmarks from localStorage
+   var bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+   //Add bookmark to array
+   bookmarks.push(bookmark);
+   //Re-set to localStorage
+   localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+
 }
 
   //Prevent form from submitting
   e.preventDefault();
+}
+
+//fetch bookmarks
+function fetchBookmarks(){
+  //GEt bookmarks from localStorage
+ var bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+
+//Get output id
+var bookmarksResults = document.getElementById('bookmarksResults');
+
+//Build output
+bookmarksResults.innerHTML = '';
+for (var i = 0; i < bookmarks.length; i++){
+  var name = bookmarks[i].name;
+  var url = bookmarks[i].url;
+
+  bookmarksResults.innerHTML += '<div class="well">'+
+                                '<h3>'+name+
+                                '</h3>'+
+                                '</div>';
+
+ }
 }
